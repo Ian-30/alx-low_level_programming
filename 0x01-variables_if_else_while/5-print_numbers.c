776761750633f5cc5a51cb8c3556f3235ce564ba
Ian-30 <iayn.munene@gmail.com>
2023-02-18 00:51:51 -0800

    Variables

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
- int a;
+int a;
 
- for (a = 0; a < 10; a++)
- {
-  printf("%d", a);
- }
- putchar('\n');
- return (0);
+for (a = 0; a < 10; a++)
+{
+printf("%d", a);
+}
+putchar('\n');
+return (0);
 }
 
