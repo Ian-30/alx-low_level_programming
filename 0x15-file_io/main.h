8f966c9c5b9e615f67dbbfbedc65cd2bf18babca
Ian-30 <iayn.munene@gmail.com>
2023-05-08 12:40:59 -0700

    Updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,17 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+typedef struct copy_struct
+{
+	char *from;
+	char *to;
+	char *buffer;
+	ssize_t rd;
+	ssize_t wt;
+	int from_file;
+	int to_file;
+} copy_struct;
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
